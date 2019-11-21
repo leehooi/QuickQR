83b97f3b22252a04acd333f175237388fb114cd2
Lee.Li <lee.li@wendywuchina.com>
2019-11-21 15:41:54 +0800

    create project

---
+++ QuickQR/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickQR
+{
+    public partial class Form1 : Form
+    {
+        private string _qrContent;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAppearance();
+            _qrContent = Clipboard.GetText();
+        }
+
+        private void InitializeAppearance()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            var size = Math.Min(this.Width, this.Height);
+            this.Width = size;
+            this.Height = size;
+            this.Text = "QuickQR";
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            PaintQR(_qrContent);
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            PaintQR(_qrContent);
+        }
+
+        private void PaintQR(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return;
+            }
+            var graphics = this.CreateGraphics();
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.L);
+            QrCode qrCode = qrEncoder.Encode(str);
+            var moduleSize = new FixedCodeSize(Math.Min((int)graphics.VisibleClipBounds.Width, (int)graphics.VisibleClipBounds.Height), QuietZoneModules.Two);
+            GraphicsRenderer render = new GraphicsRenderer(moduleSize, Brushes.Black, Brushes.White);
+            graphics.Clear(Color.White);
+            render.Draw(graphics, qrCode.Matrix);
+        }
+    }
+}
